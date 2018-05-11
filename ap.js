6e088dc890920eea45693cc0ebc2b145ebd2eed8
max77p <shanindrakumar@gmail.com>
2018-05-10 21:32:49 -0400

    update

--- ap.js
+++ ap.js
@@ -1,3 +1,28 @@
+
+
+$('.testBtn').on("click",function(event){
+    var search=$('#searchTerm').val();
+    var records=$('#numberOfRecords').val();
+    var queryURL="https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=85a9b5f65ee34104ba2b489ac87cb883"+"&q="+search+"?page=0";
+    event.preventDefault();
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      
+    console.log(response);
+});
+
+});
+
+
+
+
+
+
+
+/*
 var url = "https://api.nytimes.com/svc/suggest/v1/timestags";
 url += '?' + $.param({
  'api-key': "85a9b5f65ee34104ba2b489ac87cb883",
@@ -11,4 +36,4 @@ $.ajax({
  console.log(result);
 }).fail(function(err) {
  throw err;
-});
+});*/
